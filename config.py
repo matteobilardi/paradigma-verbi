cc4f29b2aac4d7090afbc1a7163f925d7f026b51
Matteo Bilardi <matteobilardi@outlook.com>
2019-10-22 11:22:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ load_dotenv(os.path.dirname(__file__) + "/../.env")
 
 MAIL_SERVER = os.environ.get("MAIL_SERVER")
 MAIL_PORT = int(os.environ.get("MAIL_PORT"))
-MAIL_USE_SSL = True if os.environ.get("MAIL_USE_SSL") == "true" else False
+MAIL_USE_SSL = (os.environ.get("MAIL_USE_SSL") == "true")
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 MAIL_DEFAULT_SENDER = MAIL_USERNAME
 MAIL_RECIPIENT_1 = os.environ.get("MAIL_RECIPIENT_1")
 VERBIX_API_KEY = os.environ.get("VERBIX_API_KEY")
-DEBUG = True if os.environ.get("DEBUG") == "true" else False
+DEBUG = (os.environ.get("DEBUG") == "true")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 CACHE_CONFIG = {
